bdbbdeb347f54c2b72737085c9cc0fdf2456398c
farish-jamal <farishjamal98@gmail.com>
2024-01-30 00:04:44 +0530

    response check and .env added

--- routes/response.routes.js
+++ routes/response.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const {handleOpenAiResponse, handleGetHistory} =  require("../controllers/response.controller");
+const {
+  handleOpenAiResponse,
+  handleGetHistory,
+} = require("../controllers/response.controller");
 const route = express.Router();
 
-route.post("/response",handleOpenAiResponse);
+route.post("/response", handleOpenAiResponse);
 route.get("/history", handleGetHistory);
 
 module.exports = route;
