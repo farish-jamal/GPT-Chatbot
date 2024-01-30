e1ef55871b4ad62794a9efefa1403a5a19d07053
farish-jamal <farishjamal98@gmail.com>
2024-01-30 18:23:52 +0530

    user model created

---
+++ models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+ userName:{
+  type: String,
+  unique: true,
+  require: true
+ },
+ email:{
+  type: String,
+  unique: true,
+  require: true
+ },
+ password:{
+  type: String,
+  require: true
+ },
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
